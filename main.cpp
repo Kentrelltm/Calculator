dd12a9f4b60f1b6728d06471901f982bd8733d45
Kentrelltm <78074550+Kentrelltm@users.noreply.github.com>
2021-08-17 04:25:09 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int num1, num2 ;
+    char operations;
+    cout << "====================================================\n";
+    cout << "\t\t\t\tWelcome to the Calculator\n\n" << endl;
+    cout << "====================================================\n";
+    cout << "\t\t\t\tPlease enter the first number below: \n\n"; cin >> num1;
+    cout << "\t\t1. Addition\n\t\t2. Subtraction\n\t\t3. Multiply\n\t\t4. Divide\n\n";
+    cin >> operations;
+
+
+
+}
